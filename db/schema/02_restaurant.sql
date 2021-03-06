216ef21f85b12119357202fb30e99a36ac9edc16
hansori01 <19sorihan88@gmail.com>
2021-03-06 23:29:47 +0000

    initial databsease set up

---
+++ db/schema/02_restaurant.sql
@@ -0,0 +1,29 @@
+-- Drop and recreate Widgets table (Example)
+
+DROP TABLE IF EXISTS orders, items_orders, items CASCADE;
+
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  price INTEGER NOT NULL DEFAULT 0,
+  prep_duration INTEGER NOT NULL DEFAULT 0,
+  photo_url VARCHAR(255) NOT NULL,
+  description VARCHAR(1000) NOT NULL
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  total_price INTEGER NOT NULL DEFAULT 0,
+  created_at TIMESTAMP
+);
+
+CREATE TABLE items_orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  item_id INTEGER REFERENCES items(id) ON DELETE CASCADE,
+  order_id INTEGER REFERENCES orders(id),
+  quantity INTEGER NOT NULL DEFAULT 0
+);
+
+
+
---
+++ db/seeds/02_restaurant.sql
@@ -0,0 +1,13 @@
+-- Widgets table seeds here (Example)
+INSERT INTO items (name, price, prep_duration, photo_url, description)
+VALUES ('Sprockets', 1);
+
+
+
+
+
+INSERT INTO orders (user_id, total_price, created_at)
+VALUES ();
+
+INSERT INTO items_orders (item_id, order_id, quantity)
+VALUES ();
