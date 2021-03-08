77fd6c0dc2a30aad4907c03c41f6907f82aec3e5
hansori01 <19sorihan88@gmail.com>
2021-03-08 00:41:09 +0000

    testing simple item selection query

--- db/items_queries.js
+++ db/items_queries.js
@@ -1,3 +1,4 @@
+const { database } = require('pg/lib/defaults');
 const db = require('../server');
 
 // Returns all the information of every menu item
@@ -9,10 +10,24 @@ const getItems = function () {
     // .then(data => data.rows)
 }
 
+const addItem = function (itemId) {
+// post /cart route.
+//user selects add to cart on specific item (itemID)
+return db.query(`
+SELECT name, price
+FROM items
+WHERE id = ${itemId};
+`).then(res => res.rows)
 
+//if itemID already exists with same orderID, change the quantity of item
+//creates a new order
+//console log new order info
+
+}
 
 
 
 module.exports = {
-  getItems
+  getItems,
+  addItem
 };
--- routes/cart.js
+++ routes/cart.js
@@ -6,60 +6,65 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const { addItem } = require('../db/items_queries')
 
 module.exports = (db) => {
 
   //TODO HOW DO WE IMPLEMENT LOGIC FOR ADDING TO CAR
-  router.post("/", (req, res) => {
+  router.get("/:itemId", (req, res) => {
 
-    const item = req.params.itemId;
-    const orderId = req.session.user_id;
+    const {itemId} = req.params;
 
-    db.query(`
-    INSERT INTO items_orders (item_id, order_id, quantity)
-    VALUES ();
-    `)
-      .then(data => {
-        const order = data.rows;
-        res.json({ order });
-        //const templateVars = { cart }
-        //info to send to the ejs file titled cart
-        //res.render('cart', templateVars)
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-
-
-    res.redirect("/");
+    console.log('this is the item:' + itemId)
+    addItem(itemId)
+    .then(data => console.log(data))
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
   });
-
-  //TODO View Cart
-  // router.get("/", (req, res) => {
-  //   const userId = req.session.user_id;
-  //   const
-  //   db.query(`
-  //     SELECT name, photo_url, price, FROM items
-  //     JOIN users ON
-  //     WHERE order_id = ${orderId};
-  //   `)
-  //     .then(data => {
-  //       const cart = data.rows;
-  //       // res.json({ cart });
-  //       const templateVars = { cart }
-  //       res.render('cart', templateVars)
-  //     })
-  //     .catch(err => {
-  //       res
-  //         .status(500)
-  //         .json({ error: err.message });
-  //     });
-  // });
-
-
-
   return router;
-};
+}
+    // .then(data => {
+    //   const order = data.rows;
+    //   res.json({ order });
+    //const templateVars = { cart }
+    //info to send to the ejs file titled cart
+    //res.render('cart', templateVars)
+    //     })
+    //     .catch(err => {
+    //       res
+    //         .status(500)
+    //         .json({ error: err.message });
+    //     });
+    //   res.redirect("/");
+    // });
+
+    //TODO View Cart
+    // router.get("/", (req, res) => {
+    //   const userId = req.session.user_id;
+    //   const
+    //   db.query(`
+    //     SELECT name, photo_url, price, FROM items
+    //     JOIN users ON
+    //     WHERE order_id = ${orderId};
+    //   `)
+    //     .then(data => {
+    //       const cart = data.rows;
+    //       // res.json({ cart });
+    //       const templateVars = { cart }
+    //       res.render('cart', templateVars)
+    //     })
+    //     .catch(err => {
+    //       res
+    //         .status(500)
+    //         .json({ error: err.message });
+    //     });
+    // });
+
+
+
+  //   return router;
+  // };
