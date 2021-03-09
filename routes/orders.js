4b2e4afe6aac1c01342eaa0176bdab4c6d2dd02d
SPARTAN-501 <luke.yin@alumni.ubc.ca>
2021-03-09 23:33:46 +0000

    Resolve conflicts

--- routes/orders.js
+++ routes/orders.js
@@ -3,16 +3,10 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    const userId = req.session.userId;
-    
-    if (!userId) {
-      res.redirect('/login');
-      return
-      };
   });
 
   router.post("/", (req, res) => {
-   
+
   });
 
   return router;
