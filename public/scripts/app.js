d725bc16e189836115e63a8dda972731c08f7243
SPARTAN-501 <luke.yin@alumni.ubc.ca>
2021-03-09 06:22:58 +0000

    Add cards and quantities

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,38 @@ $(() => {
     }
   });;
 });
+
+$(document).ready(function() {
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
+  // $(function() {
+
+  //   $("form div").append('<div class="inc button">+</div><div class="dec button">-</div>');
+
+  // });
+
+  $(".button").on("click", function() {
+
+    var $button = $(this);
+    var oldValue = $button.parent().find("input").val();
+
+    if ($button.text() == "+") {
+      var newVal = parseFloat(oldValue) + 1;
+    } else {
+     // Don't allow decrementing below zero
+      if (oldValue > 0) {
+        var newVal = parseFloat(oldValue) - 1;
+      } else {
+        newVal = 0;
+      }
+    }
+
+    $button.parent().find("input").val(newVal);
+
+  });
+
+});
