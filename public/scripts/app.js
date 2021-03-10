551d9ec30ac433cc224e2493170f3427bc6e41a4
SPARTAN-501 <luke.yin@alumni.ubc.ca>
2021-03-10 22:45:35 +0000

    Add app and index

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,44 +16,24 @@ $(document).ready(function() {
     return div.innerHTML;
   }
 
-  // $(function() {
-
-  //   $("form div").append('<div class="inc button">+</div><div class="dec button">-</div>');
-
-  // });
-
   // Logic for adding the subtotal
   // Hide all the pictures except for picture #1
   // Hide all the new div elements for each card except for caesar #1
   // On click, hide picture 1, hide card 1, and show picture * and card *
   // On click of ca
 
-  // $('.item-price').on('click', function(){
-  //   currentPrice = parseFloat($(this)[0].innerText);
-  //   currentPrice = Math.round(currentPrice * 100 ) / 100;
-  // });
-
   let currentPrice = 0;
 
-  // $(".open-card").on("click", function() {
-  //   //console.log($(this).children().find("innerHTML").val());
-  //   // let thisCheck = $(this);
-  //   // currentPrice = $(this)[0].children[0].children[1].children[0].innerText.val();
-  //   // console.log(currentPrice);
-
-  //    //this works
-  //   //currentPrice = parseFloat($(this)[0].children[0].children[1].children[0].innerHTML);
-  //   //currentPrice = Math.round(currentPrice * 100 ) / 100;
-
-
-
-  // });
-
   let total = 0;
+
+  let cartOB = {};
+
   $('#itemTotalPrice').text(total);
 
 
   $(".button").on("click", function() {
+
+    let id = $(this)[0].parentElement.parentElement.parentElement.children[3].innerHTML;
     let price = $(this)[0].parentElement.parentElement.parentElement.children[0].children[1].children[0].innerHTML;
     currentPrice = parseFloat(price);
     currentPrice = Math.round(currentPrice * 100 ) / 100;
@@ -71,7 +51,6 @@ $(document).ready(function() {
       if (oldValue > 0) {
         var newVal = parseFloat(oldValue) - 1;
          total -= currentPrice;
-
       } else {
         newVal = 0;
       }
@@ -80,8 +59,30 @@ $(document).ready(function() {
     $button.parent().find("input").val(newVal);
     total = Math.round(total * 100 ) / 100;
     $("#itemTotalPrice").text(total);
+    cartOB[id] = newVal;
+    window.localStorage.setItem("cart", JSON.stringify(cartOB));
 
   });
 
 
+  /*
+  $(".view-cart").on("click", function() {
+
+    // console.log(cartOB);
+    $.ajax({
+      url: "/cart",
+      method: 'POST',
+      data: cartOB
+    })
+    .then(function () {
+      console.log('Success: ');
+
+  });
+
+
+  });
+  */
+
+
 });
+
