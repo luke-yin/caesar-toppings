8e84005cea604fa890485fc87e68638f96b7462a
SPARTAN-501 <luke.yin@alumni.ubc.ca>
2021-03-10 09:47:17 +0000

    Got the total order price to work correctly

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,73 +27,61 @@ $(document).ready(function() {
   // Hide all the new div elements for each card except for caesar #1
   // On click, hide picture 1, hide card 1, and show picture * and card *
   // On click of ca
-  $('.item-price').on('click', function(){
-    console.log($(this)[0].innerText);
-  });
+
+  // $('.item-price').on('click', function(){
+  //   currentPrice = parseFloat($(this)[0].innerText);
+  //   currentPrice = Math.round(currentPrice * 100 ) / 100;
+  // });
 
   let currentPrice = 0;
 
-  $(".open-card").on("click", function() {
-    //console.log($(this).children().find("innerHTML").val());
-    // let thisCheck = $(this);
-    currentPrice = parseFloat($(this)[0].children[0].children[1].children[0].innerHTML);
-    // currentPrice = currentPrice.toFixedNumber(2);
-    currentPrice = Math.round(currentPrice * 100 ) / 100;
-    // currentPrice = currentPrice.toFixedNumber(2);
-  });
+  // $(".open-card").on("click", function() {
+  //   //console.log($(this).children().find("innerHTML").val());
+  //   // let thisCheck = $(this);
+  //   // currentPrice = $(this)[0].children[0].children[1].children[0].innerText.val();
+  //   // console.log(currentPrice);
+
+  //    //this works
+  //   //currentPrice = parseFloat($(this)[0].children[0].children[1].children[0].innerHTML);
+  //   //currentPrice = Math.round(currentPrice * 100 ) / 100;
+
+
+
+  // });
 
   let total = 0;
-  $(".itemTotalPriceTest").html(total);
+  $('#itemTotalPrice').text(total);
 
 
   $(".button").on("click", function() {
+    let price = $(this)[0].parentElement.parentElement.parentElement.children[0].children[1].children[0].innerHTML;
+    currentPrice = parseFloat(price);
+    currentPrice = Math.round(currentPrice * 100 ) / 100;
     //it has to be var not let or const in order to work
     var $button = $(this);
     var oldValue = $button.parent().find("input").val();
 
     if ($button.text() == "+") {
       var newVal = parseFloat(oldValue) + 1;
-      // $(".itemTotalPriceTest").val(this.price);
+
       total += currentPrice;
-      // $(".itemTotalPriceTest").html(total);
+
     } else {
      // Don't allow decrementing below zero
       if (oldValue > 0) {
         var newVal = parseFloat(oldValue) - 1;
          total -= currentPrice;
-        // $(".itemTotalPriceTest").html(total);
+
       } else {
         newVal = 0;
-
       }
     }
 
     $button.parent().find("input").val(newVal);
-   //let totalPrice = newVal * currentPrice;
     total = Math.round(total * 100 ) / 100;
-    console.log(total);
-    //$(".itemTotalPriceTest").html(totalPrice);
-    $(".itemTotalPriceTest").html(total);
-    console.log(newVal);
+    $("#itemTotalPrice").text(total);
 
-    // total = newVal;
-    // $("#itemTotalPrice").html(total);
   });
-
-
-
-  // Character counter
-  // $("#tweet-text").on("input", function() {
- // let length = $(this).val().length;
-  //   let counter = $(this).siblings("div").find("output");
-  //   counter.html(140-length);
-  //   if (140 - length < 0) {
-  //     counter.addClass("toggleRed");
-  //   }
-  //   else {
-  //     counter.removeClass("toggleRed");
-  //   }
-  // });
 
 
 });
